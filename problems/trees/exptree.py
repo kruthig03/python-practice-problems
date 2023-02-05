1e1b392f7e971b4128400f1b3d7f26a3b6236137
kruthig03 <114615590+kruthig03@users.noreply.github.com>
2023-02-05 12:19:57 -0600

    Update exptree.py

--- problems/trees/exptree.py
+++ problems/trees/exptree.py
@@ -14,6 +14,67 @@ class Int:
     def __str__(self):
         return str(self.n)
 
+class BinOp:
+    def __init__(self, operator, op1, op2):
+        self.op1 = op1
+        self.op2 = op2
+        self.operator = operator
+
+    def is_const(self):
+        return False
+
+    def num_nodes(self):
+        return 1 + self.op1.num_nodes() + self.op2.num_nodes()
+
+    def eval(self):
+        op1_value = self.op1.eval()
+        op2_value = self.op2.eval()
+        
+        if self.operator == '+':
+            return op1_value + op2_value
+        if self.operator == '-':
+            return op1_value - op2_value
+        if self.operator == '*':
+            return op1_value * op2_value
+        if self.operator == '/':
+            return op1_value / op2_value
+
+    def __str__(self):
+        op1_str = str(self.op1)
+        op2_str = str(self.op2)
+
+        if self.operator == '+':
+            return f"({op1_str} + {op2_str})"
+        if self.operator == '-':
+            return f"({op1_str} - {op2_str})"
+        if self.operator == '*':
+            return f"({op1_str} * {op2_str})"
+        if self.operator == '/':
+            return f"({op1_str} / {op2_str})"
+
+class BoolOp:
+    def __init__(self, operator, op1, op2):
+        self.op1 = op1
+        self.op2 = op2
+        self.operator = operator
+
+    def is_const(self):
+        return False
+
+    def num_nodes(self):
+        return 1 + self.op1.num_nodes() + self.op2.num_nodes()
+    
+    def eval(self):
+        op1_value = self.op1.eval()
+        op2_value = self.op2.eval()
+        
+        if self.operator == 'and':
+            return op1_value and op2_value
+        if self.operator == 'or':
+            return op1_value or op2_value
+        if self.operator == 'not':
+            return not op1_value
+    
 
 class Plus:
     def __init__(self, op1, op2):
